18dcc42f4649691cda2843c0c29fdc5393285d46
Reynard Timotius <91584193+Xhourstoprogram@users.noreply.github.com>
2022-03-15 15:33:49 +0700

    Add files via upload

---
+++ number1.js
@@ -0,0 +1,12 @@
+function extractprime (num){
+    if (num = 1) {
+        console.log([]) ;
+    }
+    if (num > 1) {
+        for (let i = 2; i < number; i++) {
+            
+    }
+        }
+
+}
+console.log(extractprime(1))
